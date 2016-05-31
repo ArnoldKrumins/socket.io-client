86ad18572482f41e0a4e5f4116abd7691111fde4
Arnold Krumins <arnold.krumins@gmail.com>
2016-05-31 20:07:26 +0200

    adding typings as it a bugger to get right on different machines

---
+++ typings/browser.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="browser/ambient/angular-protractor/index.d.ts" />
+/// <reference path="browser/ambient/es6-shim/index.d.ts" />
+/// <reference path="browser/ambient/jasmine/index.d.ts" />
+/// <reference path="browser/ambient/selenium-webdriver/index.d.ts" />
+/// <reference path="browser/ambient/socket.io-client/index.d.ts" />
