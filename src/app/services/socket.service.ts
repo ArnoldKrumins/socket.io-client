0c6e2c8503fdf3a8292347f1cfcc3d60db773d10
Arnold Krumins <arnold.krumins@gmail.com>
2016-11-05 17:52:11 +0100

    changing having one observable to having a subject

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -8,13 +8,18 @@ export class SocketService {
 
   private socketClient:SocketIOClient.Socket;
 
+  private subject;
+
   private connectionObservable:Observable<any>;
+  private messageObservable:Observable<any>;
+  private countObservable:Observable<any>;
 
   private  url = "http://localhost:3000";
 
 
   constructor() {
     this.socketClient = io(this.url);
+    this.subject = new Subject();
     this.init();
   }
 
@@ -23,23 +28,35 @@ export class SocketService {
 
     this.connectionObservable = new Observable(observer => {
 
-      this.socketClient.on('connection', (data) => {
+     this.socketClient.on('connection', (data) => {
+        console.log('connection',data);
         observer.next(data);
       });
+    });
 
-      this.socketClient.on('message', (data) => {
+  this.messageObservable = new Observable(observer => {
+    this.socketClient.on('message', (data) => {
+        console.log('message',data);
         observer.next(data);
       });
+  });
 
-      return () => {
-        this.socketClient.disconnect();
-      };
+    this.countObservable = new Observable(observer => {
+      this.socketClient.on('count', (data) => {
+        console.log('count',data);
+        observer.next(data);
+      });
+    });
 
-    })
+    this.connectionObservable.subscribe(this.subject);
+    this.messageObservable.subscribe(this.subject);
+    this.countObservable.subscribe(this.subject);
+    //this.connectionObservable.multicast(new Subject())
   }
 
   public communicate():Observable<any>{
-    return this.connectionObservable;
+    //return this.connectionObservable;
+    return this.subject;
   }
 
   public sendMessage(message:string):void{
